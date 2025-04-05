39204d79b7c90635603e457ec46827650f89f115
bakerdj11 <dbake418@gmail.com>
2025-04-05 11:43:26 +0900

    Update render_server.py

--- render_server.py
+++ render_server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # ✅ Enable CORS for all routes and origins
+
 colab_url = None  # Global variable to store the received URL
 
 @app.route('/')
